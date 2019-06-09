7d16f827081ed43a9ff46fe25c4658c501a2db90
matin <matin.zamani99@gmail.com>
2019-06-09 11:17:50 +0430

    first triangle class tests

---
+++ AT-810197520-triangle_test.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "Triangle.hpp"
+using namespace std;
+
+TEST_CASE("A triangle with no value for sides")
+{   
+    Triangle t();
+    REQUIRE_THROWS_AS(t,std::invalid_argument);
+}
+TEST_CASE("A triangle with one zero side")
+{
+    REQUIRE_THROWS_AS(Triangle(0,2,3),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(2,0,3),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(2,3,0),std::invalid_argument);
+}
+TEST_CASE("A triangle with not valid arguments")
+{
+    REQUIRE_THROWS_AS(Triangle(1,2,3),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(1,3,2),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(2,1,3),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(2,3,1),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(3,2,1),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(3,1,2),std::invalid_argument);
+}
+TEST_CASE("A triangle with a negative value")
+{
+    REQUIRE_THROWS_AS(Triangle(-1,3,4),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(-1,4,3),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(3,-1,4),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(4,-1,3),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(3,4,-1),std::invalid_argument);
+    REQUIRE_THROWS_AS(Triangle(4,3,-1),std::invalid_argument);
+}
+TEST_CASE("get primeter of a simple triangle")
+{
+    Triangle t(5,2,4);
+    REQUIRE(t.get_perimeter()==11);
+}
+TEST_CASE("get kind of a simple triangle")
+{
+    Triangle t(5,2,4);
+    REQUIRE(t.get_kind()==Triangle::Kind::SCALENE);
+}
+TEST_CASE("get area of a simple triangle")
+{
+    Triangle t(5,2,4);
+    REQUIRE(t.get_area()==3.79967);
+} 
+TEST_CASE("get kind of a ISOSCELES triangle")
+{
+    REQUIRE(Triangle(3,3,5).get_kind()==Triangle::Kind::ISOSCELES);
+    REQUIRE(Triangle(5,3,3).get_kind()==Triangle::Kind::ISOSCELES);
+    REQUIRE(Triangle(3,5,3).get_kind()==Triangle::Kind::ISOSCELES);
+}
+TEST_CASE("get kind of a EQUILATERAL triangle")
+{
+    REQUIRE(Triangle(3,3,3).get_kind()==Triangle::Kind::EQUILATERAL);
+}
+TEST_CASE("get area of a ISOSCELES triangle")
+{
+    REQUIRE(Triangle(3,3,5).get_area()==4.14578);
+}
+TEST_CASE("get area of a EQUILATERAL triangle")
+{
+    REQUIRE(Triangle(3,3,3).get_area()==3.89711);
+}
+TEST_CASE("is_isosceles test")
+{
+    class TriangleUnderTest : private Triangle
+    {
+        public:
+            TriangleUnderTest(int _side1,int _side2,int _side3) : Triangle(_side1,_side2,_side3){}
+            bool get_if_isosceles(){return is_isosceles();}
+    };
+    REQUIRE(TriangleUnderTest(1,3,3).get_if_isosceles()==true);
+    REQUIRE(TriangleUnderTest(3,1,3).get_if_isosceles()==true);
+    REQUIRE(TriangleUnderTest(3,3,1).get_if_isosceles()==true);
+}
+TEST_CASE("is_equilateral test")
+{
+    class TriangleUnderTest : public Triangle
+    {
+        public:
+            TriangleUnderTest(int _side1,int _side2,int _side3) : Triangle(_side1,_side2,_side3){}
+            bool get_if_equilateral(){return is_equilateral();}
+    };
+    REQUIRE(TriangleUnderTest(3,3,3).get_if_equilateral()==true);
+}
