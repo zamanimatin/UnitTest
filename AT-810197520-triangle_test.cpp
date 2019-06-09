0edc39c9e6da77ce0760353909a3614b2f1876ac
zamanimatin <49686786+zamanimatin@users.noreply.github.com>
2019-06-09 11:43:49 +0430

    triangle class final tests

--- AT-810197520-triangle_test.cpp
+++ AT-810197520-triangle_test.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <cmath>
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "Triangle.hpp"
@@ -63,13 +64,13 @@ TEST_CASE("get kind of a EQUILATERAL triangle")
 }
 TEST_CASE("get area of a ISOSCELES triangle")
 {
-    REQUIRE(Triangle(3,3,5).get_area()==4.14578);
+    REQUIRE(Triangle(4,4,6).get_area()==3*sqrt(7));
 }
 TEST_CASE("get area of a EQUILATERAL triangle")
 {
-    REQUIRE(Triangle(3,3,3).get_area()==3.89711);
+    REQUIRE(Triangle(4,4,4).get_area()==4*sqrt(3));
 }
-TEST_CASE("is_isosceles test")
+TEST_CASE("is_isosceles test for true answers")
 {
     class TriangleUnderTest : private Triangle
     {
@@ -81,7 +82,17 @@ TEST_CASE("is_isosceles test")
     REQUIRE(TriangleUnderTest(3,1,3).get_if_isosceles()==true);
     REQUIRE(TriangleUnderTest(3,3,1).get_if_isosceles()==true);
 }
-TEST_CASE("is_equilateral test")
+TEST_CASE("is_isosceles test to check for false answers")
+{
+    class TriangleUnderTest : private Triangle
+    {
+        public:
+            TriangleUnderTest(int _side1,int _side2,int _side3) : Triangle(_side1,_side2,_side3){}
+            bool get_if_isosceles(){return is_isosceles();}
+    };
+    REQUIRE(TriangleUnderTest(2,3,4).get_if_isosceles()==false);
+}
+TEST_CASE("is_equilateral test for true answers")
 {
     class TriangleUnderTest : public Triangle
     {
@@ -90,4 +101,17 @@ TEST_CASE("is_equilateral test")
             bool get_if_equilateral(){return is_equilateral();}
     };
     REQUIRE(TriangleUnderTest(3,3,3).get_if_equilateral()==true);
+}
+TEST_CASE("is_equilateral test for false answers")
+{
+    class TriangleUnderTest : public Triangle
+    {
+        public:
+            TriangleUnderTest(int _side1,int _side2,int _side3) : Triangle(_side1,_side2,_side3){}
+            bool get_if_equilateral(){return is_equilateral();}
+    };
+    REQUIRE(TriangleUnderTest(2,3,4).get_if_equilateral()==false);
+    REQUIRE(TriangleUnderTest(3,3,4).get_if_equilateral()==false);
+    REQUIRE(TriangleUnderTest(3,4,3).get_if_equilateral()==false);
+    REQUIRE(TriangleUnderTest(4,3,3).get_if_equilateral()==false);
 }
